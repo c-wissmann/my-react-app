dc7ca28ec590fa1988e8903f8593b9b5c3a65c92
c-wissmann <c.t.wissmann@wustl.edu>
2024-11-09 15:43:39 -0600

    changed 'square' to 'squares' in stateVariable declaration in 'Board'

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function Square() {
 }
 
 export default function Board() {
-  const [square, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
